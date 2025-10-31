f609bd3beea553c95625a57290079926301c4530
Bulhan-Maalin <maalin.622140@swl.k12.oh.us>
2025-10-31 08:36:14 -0400

    made getDouble, getPositiveNonZeroInt, getYNConfirm, and getRangedDouble methods.

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int numGuesser = InputHelper.getRangedInt(scan, "Guess a number between 1 and 10:", 1, 10);
-        System.out.println("You guessed " + numGuesser);
+
+
+        boolean YorN = InputHelper.getYNConfirm(scan, "Enter Y if you'd like to keep playing or N if you'd like to stop:");
+        System.out.println(YorN);
     }
 }
